074fa9f4d6d53b553b22bdb9dbec240b18e31cfc
Arleth Ontiveros <aontiveros@nyc.yearup.org>
2018-11-16 15:25:06 -0500

    pokemon

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 axios.get("http://fizal.me/pokeapi/api/v2/id/25.json")
 .then(function(response){
-  console.log(response);
+  console.log(response.data.abilities[0].ability.name);
 })
+var myImage = document.getElementById('card_one');
+myImage.addEventListener('click', showAnimation);
+
+function showAnimation(){
+  setTimeout(function (){
+    myImage.style.animation = "flip 2s linear";
+  },1);
+}
+
+// My Pokemon Super Class / Main Class
+class Pokemon{
+  constructor(){
+
+  }
+}
+// My Pokemon Object goes here
+class MyPokemon extends Pokemon{
+
+}
+// my Trainer object goes here
+class Trainer{
+
+}
