44c3d77dfe914a426bb907ccc413deb17ff4c5a2
Arleth Ontiveros <aontiveros@nyc.yearup.org>
2018-11-20 16:48:23 -0500

    pokemon

--- main.js
+++ main.js
@@ -1,43 +1,33 @@
+console.log();
+
+class pokemon{
+  constructor(name,pokeLife,pokeAttack, pokeDefense,pokeAbilities){}
+}
+
+
 axios.get("http://fizal.me/pokeapi/api/v2/id/25.json")
 .then(function(response){
   console.log(response);
   console.log(response.data.abilities[0].ability.name);
 })
-var myImage = document.getElementById('card_one');
-myImage.addEventListener('click',  showAnimation());
+let myImage = document.getElementById('card_one');
+  myImage.addEventListener('click',  function(){
+  myImage.style.animation = "flip 2s"
+});
+let myImage2 = document.getElementById('card_two');
+  myImage2.addEventListener('click', function(){
+  myImage2.style.animation = "flip 2s"
+});
 
-// var myImage2 = document.getElementById('card_two');
-// myImage2.addEventListener('click', showAnimation);
-//
-// var myImage3 = document.getElementById('card_three');
-// myImage3.addEventListener('click', showAnimation);
+let myImage3 = document.getElementById('card_three');
+  myImage3.addEventListener('click', function(){
+  myImage3.style.animation = "flip 2s"
+});
 
-function showAnimation(){
-  setTimeout(function (){
-    myImage.style.animation = "flip 2s linear";
-  },100);
-}
-
-// var myImage = document.getElementById('card_two');
-// myImage.addEventListener('click', showAnimation);
-
-// function showAnimation(){
-//   setTimeout(function (){
-//     myImage.style.animation = "flip 2s linear";
-//   },1);
-// }
-
-
-
-// function showAnimation(){
-//   setTimeout(function(){
-//     myImage.style.animation = "flip 2s linear"
-//   },1);
-// }
 
 
 // My Pokemon Super Class / Main Class
-class Pokemon{
+class Pokemon(){
   constructor(){
 
   }
@@ -48,5 +38,5 @@ class MyPokemon extends Pokemon{
 }
 // my Trainer object goes here
 class Trainer{
-
-}
+//
+// }
